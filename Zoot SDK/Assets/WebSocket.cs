ea455eb4755b7aaa474b4caec31226f9b86ce23c
Pavelescu Razvan <pavelescu.razvan@gmail.com>
2024-05-14 14:30:07 +0300

    no message

--- Zoot SDK/Assets/WebSocket.cs
+++ Zoot SDK/Assets/WebSocket.cs
@@ -13,20 +13,21 @@ public class SocketManager : MonoBehaviour
     public string Path = "/crash";
 
     public string GameRoundUuid = "your_game_round_uuid";
+    public int playAmount = 1;
+    public int coinTypeId = 0;
 
     public TMPro.TextMeshProUGUI CurrentGameRoundStatus;
 
 
-    //
-    // Server Actions (from server towards client)
-    //
+    /**
+     * Server Actions (from server towards client)
+     */
     const string WELCOME_FROM_SERVER = "WELCOME_FROM_SERVER";
 
     // Game round status
     const string GAME_ROUND_PREPARED = "GAME_ROUND_PREPARED";
     const string GAME_ROUND_LIVE = "GAME_ROUND_LIVE";
     const string GAME_ROUND_COMPLETED = "GAME_ROUND_COMPLETED";
-    const string GAME_ROUND_CANCELED = "GAME_ROUND_CANCELED";
 
     const string USER_BET_LIST_SUCCEEDED = "USER_BET_LIST_SUCCEEDED";
     const string USER_BET_LIST_FAILED = "USER_BET_LIST_FAILED";
@@ -35,20 +36,15 @@ public class SocketManager : MonoBehaviour
     const string BET_REGISTER_SUCCEEDED = "BET_REGISTER_SUCCEEDED";
     const string BET_REGISTER_FAILED = "BET_REGISTER_FAILED";
 
-    // Deregister events (confirming play deregister action)
-    const string BET_DEREGISTER_SUCCEEDED = "BET_DEREGISTER_SUCCEEDED";
-    const string BET_DEREGISTER_FAILED = "BET_DEREGISTER_FAILED";
-
     // Cashout events (confirming play cashout action)
     const string BET_CASHOUT_SUCCEEDED = "BET_CASHOUT_SUCCEEDED";
     const string BET_CASHOUT_FAILED = "BET_CASHOUT_FAILED";
 
 
-    //
-    // Client Actions (from client towards server)
-    //
+    /**
+     * Client Actions (from client towards server) 
+     */
     const string BET_REGISTER = "BET_REGISTER";
-    const string BET_DEREGISTER = "BET_DEREGISTER";
     const string BET_CASHOUT = "BET_CASHOUT";
     const string BET_LIST = "BET_LIST";
 
@@ -78,7 +74,7 @@ public class SocketManager : MonoBehaviour
 
         client.OnConnected += (sender, e) =>
         {
-            Debug.Log("Connected to server");
+            Debug.Log("Connected to Socket server");
 
             SetupListeners();
         };
@@ -86,75 +82,139 @@ public class SocketManager : MonoBehaviour
         client.ConnectAsync();
     }
 
+    /**
+     * Responsible with listening to events incoming from the server towards the client
+     */
     private void SetupListeners()
     {
-        Debug.Log("Listeners mounted");
 
+        /**
+         * After the client sends an event towards the server,
+         * the server will respond with a Success or Failture event
+         * E.g. BET_REGISTER_SUCCEEDED or BET_REGISTER_FAILED
+         */
         client.On(BET_REGISTER_SUCCEEDED, response =>
         {
-            Debug.Log("Bet register succeeded: " + response.ToString());
+            Debug.Log("Incoming BET_REGISTER_SUCCEEDED:" + response.ToString());
         });
 
         client.On(BET_REGISTER_FAILED, response =>
         {
-            Debug.Log("Bet register failed: " + response.ToString());
+            Debug.Log("Incoming BET_REGISTER_FAILED:" + response.ToString());
         });
 
-        
+        client.On(BET_CASHOUT_SUCCEEDED, response =>
+        {
+            Debug.Log("Incoming BET_CASHOUT_SUCCEEDED:" + response.ToString());
+        });
+
+        client.On(BET_CASHOUT_FAILED, response =>
+        {
+            Debug.Log("Incoming BET_CASHOUT_FAILED:" + response.ToString());
+        });
+
+        client.On(USER_BET_LIST_SUCCEEDED, response =>
+        {
+            Debug.Log("Incoming USER_BET_LIST_SUCCEEDED:" + response.ToString());
+        });
+
+        client.On(USER_BET_LIST_FAILED, response =>
+        {
+            Debug.Log("Incoming USER_BET_LIST_FAILED:" + response.ToString());
+        });
+
+
+        /**
+         * Upon connecting to the Socket server, the server will send a
+         * welcome event containnig the current status of the game round
+         */
         client.On(WELCOME_FROM_SERVER, response =>
         {
-            Debug.Log("response listener: " + response.ToString());
+            Debug.Log("Incoming WELCOME_FROM_SERVER with GameRoundStatus: " + response.ToString());
+
 
             CurrentGameRoundStatus.SetText(WELCOME_FROM_SERVER);
         });
 
+        /**
+         * The server sends game round status updates
+         */
         client.On(GAME_ROUND_PREPARED, response =>
         {
-            Debug.Log("response listener: " + response.ToString());
+            Debug.Log("Incoming GameRoundStatus: GAME_ROUND_PREPARED");
 
             CurrentGameRoundStatus.SetText(GAME_ROUND_PREPARED);
         });
 
         client.On(GAME_ROUND_LIVE, response =>
         {
-            Debug.Log("response listener: " + response.ToString());
+            Debug.Log("Incoming GameRoundStatus: GAME_ROUND_LIVE");
 
             CurrentGameRoundStatus.SetText(GAME_ROUND_LIVE);
         });
 
         client.On(GAME_ROUND_COMPLETED, response =>
         {
-            Debug.Log("response listener: " + response.ToString());
+            Debug.Log("Incoming GameRoundStatus: GAME_ROUND_COMPLETED");
 
             CurrentGameRoundStatus.SetText(GAME_ROUND_COMPLETED);
         });
 
-        client.On(GAME_ROUND_CANCELED, response =>
-        {
-            Debug.Log("response listener: " + response.ToString());
-
-            CurrentGameRoundStatus.SetText(GAME_ROUND_CANCELED);
-        });
+        Debug.Log("Listeners mounted");
     }
 
+    // Register a bet register action within a gameRound
     public async void BetRegister()
     {
         Debug.Log("Clicked BetRegister");
 
-        // Define the payload to send
         var payload = new Dictionary<string, object>
         {
             { "gameRoundUuid", GameRoundUuid },
             { "userId", UserId },
             { "userNickname", "Richard" },
-            { "playAmountInCents", 100 },
-            { "coinType", 0 },
+            { "playAmountInCents", playAmount * 100 },
+            { "coinType", coinTypeId },
             { "pictureUrl", "https://lh3.googleusercontent.com/a/ACg8ocLyp0TCe7yq2ydJJm3d32XgcP3yh8T2wEXBHL4zW2dk=s96-c" },
             { "userAccessToken", UserAccessToken },
         };
 
         // Send the event to the server with the payload
         await client.EmitAsync(BET_REGISTER, payload);
+    }
+
+    // Register a bet cashout action within a gameRound
+    public async void BetCashout()
+    {
+        Debug.Log("Clicked BetCashout");
+
+        var payload = new Dictionary<string, object>
+        {
+            { "gameRoundUuid", GameRoundUuid },
+            { "userId", UserId },
+            { "userNickname", "Richard" },
+            { "userAccessToken", UserAccessToken },
+        };
+
+        // Send the event to the server with the payload
+        await client.EmitAsync(BET_CASHOUT, payload);
+    }
+
+    // Request from server the list of all registered bets within a gameRound
+    public async void BetList()
+    {
+        Debug.Log("Clicked BetList");
+
+        var payload = new Dictionary<string, object>
+        {
+            { "gameRoundUuid", GameRoundUuid },
+            { "userId", UserId },
+            { "userNickname", "Richard" },
+            { "userAccessToken", UserAccessToken },
+        };
+
+        // Send the event to the server with the payload
+        await client.EmitAsync(BET_LIST, payload);
     }
 
 
