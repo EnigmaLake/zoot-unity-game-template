e662976477eeda65510a202224ccf02c5c885796
Pavelescu Razvan <pavelescu.razvan@gmail.com>
2024-05-14 12:59:28 +0300

    no message

--- Zoot SDK/Assets/WebSocket.cs
+++ Zoot SDK/Assets/WebSocket.cs
@@ -12,6 +12,32 @@ public class SocketManager : MonoBehaviour
     public string GuestUserId = "your_guest_user_id";
     public string Path = "/crash";
 
+    // Server Actions (from server towards client)
+    const string WELCOME_FROM_SERVER = "WELCOME_FROM_SERVER";
+
+    // Game round status
+    const string GAME_ROUND_PREPARED = "GAME_ROUND_PREPARED";
+    const string GAME_ROUND_LIVE = "GAME_ROUND_LIVE";
+    const string GAME_ROUND_COMPLETED = "GAME_ROUND_COMPLETED";
+    const string GAME_ROUND_CANCELED = "GAME_ROUND_CANCELED";
+
+    const string USER_BET_LIST_SUCCEEDED = "USER_BET_LIST_SUCCEEDED";
+    const string USER_BET_LIST_FAILED = "USER_BET_LIST_FAILED";
+
+    // Register events (confirming register action)
+    const string BET_REGISTER_SUCCEEDED = "BET_REGISTER_SUCCEEDED";
+    const string BET_REGISTER_FAILED = "BET_REGISTER_FAILED";
+
+    // Deregister events (confirming deregister action)
+    const string BET_DEREGISTER_SUCCEEDED = "BET_DEREGISTER_SUCCEEDED";
+    const string BET_DEREGISTER_FAILED = "BET_DEREGISTER_FAILED";
+
+    // Cashout events (confirming cashout action)
+    const string BET_CASHOUT_SUCCEEDED = "BET_CASHOUT_SUCCEEDED";
+    const string BET_CASHOUT_FAILED = "BET_CASHOUT_FAILED";
+
+
+    // Client Actions (from client towards server)
     const string BET_REGISTER = "BET_REGISTER";
     const string BET_DEREGISTER = "BET_DEREGISTER";
     const string BET_CASHOUT = "BET_CASHOUT";
@@ -39,9 +65,26 @@ public class SocketManager : MonoBehaviour
         client.OnConnected += (sender, e) =>
         {
             Debug.Log("Connected to server");
+
+            SetupListeners();
         };
 
         client.ConnectAsync();
+    }
+
+    private void SetupListeners()
+    {
+        Debug.Log("Listeners mounted");
+
+        client.On(BET_REGISTER_SUCCEEDED, response =>
+        {
+            Debug.Log("Bet register succeeded: " + response.ToString());
+        });
+
+        client.On(BET_REGISTER_FAILED, response =>
+        {
+            Debug.Log("Bet register failed: " + response.ToString());
+        });
     }
 
     public async void BetRegister()
@@ -63,6 +106,7 @@ public class SocketManager : MonoBehaviour
         // Send the event to the server with the payload
         await client.EmitAsync(BET_REGISTER, payload);
     }
+
 
     void OnDestroy()
     {
